1d83078c008ea5819323a0bd8859a3a58b37cdcc
nagase <yoshimasa_nagase@tsyscom.co.jp>
2017-09-29 22:40:03 +0900

    起動できた

--- spring-fast-startup/src/main/java/com/naggi/springboot/lazyinit/LazyApplication.java
+++ spring-fast-startup/src/main/java/com/naggi/springboot/lazyinit/LazyApplication.java
@@ -2,6 +2,9 @@ package com.naggi.springboot.lazyinit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 import com.naggi.springboot.lazyinit.controller.GreetingRestController;
@@ -9,22 +12,23 @@ import com.naggi.springboot.lazyinit.data.Greeting;
 import com.naggi.springboot.lazyinit.service.EagerLoadService;
 
 
-//@SpringBootApplication
-
-@ComponentScan(basePackageClasses = LazyApplication.class,lazyInit=true)
+@SpringBootApplication
+//@ComponentScan(basePackageClasses = LazyApplication.class,lazyInit=true)
 //@ComponentScan(basePackageClasses = LazyApplication.class)
-@EnableAutoConfiguration(
-)
-public class LazyApplication {
+public class LazyApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LazyApplication.class);
+    }
+    
     public static void main(String[] args) {
         SpringApplication.run(LazyApplication.class, args);
     }
-
+    
 //    @Configuration
 //    @Profile("local")
 //    @ComponentScan(lazyInit = true)
 //    static class LocalConfig {
 //    }
-
 }
