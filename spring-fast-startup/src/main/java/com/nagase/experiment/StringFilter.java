26781a75c24c9ca409c55b460dc201d5e768e2d8
nagase <yoshimasa_nagase@tsyscom.co.jp>
2017-10-20 18:46:27 +0900

    テストパターンを修正した。

--- spring-fast-startup/src/main/java/com/nagase/experiment/StringFilter.java
+++ spring-fast-startup/src/main/java/com/nagase/experiment/StringFilter.java
@@ -8,8 +8,10 @@ public class StringFilter {
 
     
     public boolean doFilter(String str) {
-
-        return str.matches(".*_(flg|flg_\\d+).*$");
+        //xxx_flg or flg+数字列 or xxxx_flg_xxx or 
+        return str.toLowerCase().matches("(flg_.*|.*_flg_.*|.*_flg)$");
     }
+    
+   // matches(".*_(flg|flg_\\d+)$")) 
 
 }
--- spring-fast-startup/src/test/java/com/nagase/experiment/StringFilterSpec.groovy
+++ spring-fast-startup/src/test/java/com/nagase/experiment/StringFilterSpec.groovy
@@ -18,14 +18,18 @@ class StringFilterSpec extends Specification{
     def "フィルタに一致するパターンのテスト"() {
         expect:
         instance.doFilter(a) == true
-
 		
         where:
 		a | b
-        "a_flg" | 2
-		"a_flg" | 2
-		"a_flg" | 2
-		
+        "a_flg_3" | 2
+		"a_flg_223" | 2
+        "a_flg_x" | 2
+        "xxxx_33x_xxxx_flg" | 2
+        "xxxx_flg_xxx" | 2
+        "xxxx_flg_" | 2
+        "flg_xxxxxxxx" | 2
+        "_flg" | 2
+        "flg_" | 2
     }
 
     def "フィルターに一致しないパターンのテスト"() {
@@ -35,7 +39,14 @@ class StringFilterSpec extends Specification{
         where:
 		a | b
         "a_fleg" | 2
-		"a_fklge" | 2
+		"flga_fklflg_ge" | 2
 		"a_fl_gee" | 2
+        "a_fl_gee" | 2
+        "aflgflggee" | 2
+        "aflg_ee" | 2
+        "flgxxjdskf" | 2
+        "xxxxflg_xx" | 2
+        "xxxx_flgxxxxx" | 2
+
     }
 }
